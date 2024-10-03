72d57d507937a25e3f5486b44c618708844276a4
venfoxdj <carlos.ceballost@gmail.com>
2024-10-03 18:13:57 -0400

    model añadido

--- posts/models copy.py
+++ posts/models copy.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models (tablas de la base de datos)
 class publication(models.Model):
     text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.text[:50]
